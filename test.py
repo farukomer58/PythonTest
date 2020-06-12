caa5ac22ea6e37d16f3d29375ecef59ff976159a
Omer Citik <omer.citik@hva.nl>
2020-06-12 18:49:04 +0200

    Formatted Strings -

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-email = ''' 
+first = "Omer"
+last = "Citik"
 
-Beste Omer,
+msg = f"{first} [{last}] is idk, this is a formatted string."
 
-Je bent een k*t mogool.
-
-Met vriendelijke groet,
-Omer
-
-'''
-
-string = "Python"
-stringPart = string[0:3] #Get the character from 0 tot 2 (the 3th char is not included)
-stringPart2 = string[0] #Get first character
-stringPart3 = string[-0] #Get last character
-stringPart4 = string[1:] #Get the whole string but exlude the character till one (so the first char)
-stringCopy = string[:] #copy the string
-
-name = "Jennifer"
-namePart = name[1:-1]
-
-print(namePart)
+print(msg)
