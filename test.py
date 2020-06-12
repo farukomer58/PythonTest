7a1d1e73901d49d871e69fd4fc80c8997da5d878
Omer Citik <omer.citik@hva.nl>
2020-06-12 18:46:05 +0200

    Strings - Chars from string etc.

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-CONVERSION = 0.45359237
+email = ''' 
 
-weight = input("What is your weight in Pounds? ")
-weightKilograms = float(weight) * CONVERSION
+Beste Omer,
 
-#weightType = type(weight)
-#weightTypeKilograms = type(weightKilograms)
-#print(weightType)
-#print(weightTypeKilograms)
+Je bent een k*t mogool.
 
-print("You weight in kilograms(kg) is: " + str(weightKilograms))
+Met vriendelijke groet,
+Omer
+
+'''
+
+string = "Python"
+stringPart = string[0:3] #Get the character from 0 tot 2 (the 3th char is not included)
+stringPart2 = string[0] #Get first character
+stringPart3 = string[-0] #Get last character
+stringPart4 = string[1:] #Get the whole string but exlude the character till one (so the first char)
+stringCopy = string[:] #copy the string
+
+name = "Jennifer"
+namePart = name[1:-1]
+
+print(namePart)
