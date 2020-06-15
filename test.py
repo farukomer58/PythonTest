fcb8d2b2306bb7738953e20849ea240e6037970f
Omer Citik <omer.citik@hva.nl>
2020-06-15 03:20:30 +0200

    Arithmetic Operator and Operetor order

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-course = "Python for beginners"
-
-stringLenght = len(course)
-stringFun = course.upper()
-stringFun2 = course.lower()
-stringFun3 = course.find("for") # returns the index where he finds for the first time
-stringFun4 = course.replace("beginners", "absolute beginners")
-stringFun5 = course.title() # Make each first word uppercase
-stringCheck = "Python" in course
-
-print(stringFun5)
-
+print(10/3)
+print(10//3) #returns a int
+print(10**3) #10^3
+x = 10
+x+=3 #Augmenta Operator?
