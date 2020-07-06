c952bf2a9298b842d75bcdaf7b15788d87af9f6f
Omer Citik <omer.citik@hva.nl>
2020-07-07 00:20:13 +0200

    Simple Calculator Application

--- test.py
+++ test.py
@@ -1,25 +1,19 @@
-command = ""
-isCarStarted = False
 while True:
-        command = input("> ").lower()
+    numberA = float(input("Number A: "))
+    numberCalcMethod = input("Do you want to * - + or / : ")
+    numberB = float(input("Number B: "))
+    result = 0
+    if len(numberCalcMethod) > 0:
+        if numberCalcMethod == "*"  or numberCalcMethod == "/":
+            result = numberA * numberB
+        elif numberCalcMethod == "-":
+            result = numberA - numberB
+        elif numberCalcMethod == "+":
+            result = numberA + numberB
+        elif numberCalcMethod == "/":
+            result = numberA / numberB
 
-        if command == "start":
-            if isCarStarted:
-                print("Car is alreadyy STARTED....")
-            else:
-                print("Car started....")
-                isCarStarted = True
-        elif command == "stop":
-            if not isCarStarted:
-                print("Car is alreadyy STOPED....")
-            else:
-                print("Car stoped....")
-                isCarStarted = False
-        elif command == "help":
-            print("start - start the car")
-            print("stop - stop the car")
-            print("quit - to exit")
-        elif command == "quit":
-            break
-        else:
-            print("I dont understand that")
+    else:
+        print("ERROR: You should specify what you want to do With the Numbers")
+        break
+    print(f"The result is: {result}")
