c3376ab56a26838513d70c2b7199a42c83bda316
Omer Citik <omer.citik@hva.nl>
2020-07-07 00:23:43 +0200

    For loop

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
-while True:
-    numberA = float(input("Number A: "))
-    numberCalcMethod = input("Do you want to * - + or / : ")
-    numberB = float(input("Number B: "))
-    result = 0
-    if len(numberCalcMethod) > 0:
-        if numberCalcMethod == "*"  or numberCalcMethod == "/":
-            result = numberA * numberB
-        elif numberCalcMethod == "-":
-            result = numberA - numberB
-        elif numberCalcMethod == "+":
-            result = numberA + numberB
-        elif numberCalcMethod == "/":
-            result = numberA / numberB
-
-    else:
-        print("ERROR: You should specify what you want to do With the Numbers")
-        break
-    print(f"The result is: {result}")
+for i in range(5,10):
+    print(i)
