9249b533ed40c5b172bc456ae4a4d17e19df23f8
Omer Citik <omer.citik@hva.nl>
2020-07-09 00:24:22 +0200

    Dictonaries

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-coordinates = (1,2,4)
-x,y,z = coordinates
+customer = {
+    "name" : "Omer Citik",
+    "age" : 100,
+    "is_good": True
+}
 
-print(z)
+customer["idk"] = "new Value"
+print(customer["name"])
+print(customer["idk"])
+print(customer.get("birthday", "01-02-1999"))
